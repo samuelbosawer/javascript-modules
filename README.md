479c27f75a74350340f83d117f3dfc728a028783
Samuel Bosawer <zam.bosawer@gmail.com>
2024-02-18 22:25:52 +0900

    Import & Export

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript-modules
 Belajar JavaScript Modules
+
+```
+JavaScript Modul dibutuhan agar tidak bentrok apabila setiap variabel memiliki nilai yang sama pada file js yg berbeda
+```
---
+++ scripts/say.js
@@ -0,0 +1,11 @@
+export function sayHello(name)
+{
+    console.info(`Hello ${name}`);
+};
+
+export function sayGoodBye(name)
+{
+    console.info(`Good Bye ${name}`);
+};
+
+export const sam = 'Sam';
